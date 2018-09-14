5d284c708b3c443b9c7e812b1a1222a4579f3750
Brian Heim <brianlheim@gmail.com>
2018-09-14 00:17:17 -0500

    add soundfile copy implementation

--- src/sf2float/main.cpp
+++ src/sf2float/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "util/simple_options.hpp"
 
-bool do_copy( std::string, std::string ) {
+#include "sndfile.hh"
+
+bool do_copy( std::string from_path, std::string to_path ) {
+    SndfileHandle from{ from_path, SFM_READ };
+    if ( !from ) {
+        std::cout << "Could not open read file: " << from_path << std::endl;
+        return false;
+    }
+
+    SndfileHandle to{ to_path, SFM_WRITE, SF_FORMAT_FLOAT, from.channels(), from.samplerate() };
+    if ( !to ) {
+        std::cout << "Could not open write file: " << to_path << std::endl;
+        return false;
+    }
+
+    std::vector<float> floats( from.channels() );
+
+    while ( from.readf( floats.data(), 1 ) ) {
+        auto written = to.writef( floats.data(), 1 );
+        if ( written != 1 ) {
+            std::cout << "Error while writing" << std::endl;
+            return false;
+        }
+    }
+
+    if ( to.frames() != from.frames() ) {
+        std::cout << "Could not read entier file" << std::endl;
+        return false;
+    }
+
     return true;
 }
 
