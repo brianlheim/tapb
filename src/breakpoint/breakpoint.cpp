848761992f4b8798e6f6940c28ab3e95e0e77132
Brian Heim <brianlheim@gmail.com>
2019-11-16 17:06:35 -0600

    column is pointer

--- src/breakpoint/breakpoint.cpp
+++ src/breakpoint/breakpoint.cpp
@@ -56,36 +56,36 @@ std::variant<std::vector<point>, parse_error> parse_breakpoints( std::istream & 
                                 line_count };
 
         // eat optional whitespace
-        unsigned column = 0;
-        while ( line[column] != '\0' && ( line[column] == ' ' || line[column] == '\t' ) )
+        const char * column = line;
+        while ( *column != '\0' && ( *column == ' ' || *column == '\t' ) )
             column++;
 
-        if ( line[column] == '\0' )
+        if ( *column == '\0' )
             continue; // skip empty lines
 
         point this_point;
         char * time_end;
-        this_point.time_secs = std::strtod( line + column, &time_end );
-        if ( time_end == line + column || this_point.time_secs == HUGE_VAL )
+        this_point.time_secs = std::strtod( column, &time_end );
+        if ( time_end == column || this_point.time_secs == HUGE_VAL )
             return parse_error{ parse_error::misformatted_line, line_count };
-        column = time_end - line;
+        column = time_end;
 
-        while ( line[column] != '\0' && ( line[column] == ' ' || line[column] == '\t' ) )
+        while ( *column != '\0' && ( *column == ' ' || *column == '\t' ) )
             column++;
 
-        if ( line + column == time_end )
+        if ( column == time_end )
             return parse_error{ parse_error::misformatted_line, line_count };
 
         char * value_end;
-        this_point.value = std::strtod( line + column, &value_end );
-        if ( value_end == line + column || this_point.value == HUGE_VAL )
+        this_point.value = std::strtod( column, &value_end );
+        if ( value_end == column || this_point.value == HUGE_VAL )
             return parse_error{ parse_error::misformatted_line, line_count };
-        column = value_end - line;
+        column = value_end;
 
-        while ( line[column] != '\0' && ( line[column] == ' ' || line[column] == '\t' ) )
+        while ( *column != '\0' && ( *column == ' ' || *column == '\t' ) )
             column++;
 
-        if ( line[column] != '\0' )
+        if ( *column != '\0' )
             return parse_error{ parse_error::misformatted_line, line_count };
 
         result.push_back( this_point );
