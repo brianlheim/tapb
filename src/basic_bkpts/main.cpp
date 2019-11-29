3ab8878a5172b6432648efdd28da93a49a622d94
Brian Heim <brianlheim@gmail.com>
2019-11-29 16:12:42 -0600

    basic_bkpts: reorg

--- src/basic_bkpts/main.cpp
+++ src/basic_bkpts/main.cpp
@@ -4,6 +4,25 @@
 
 #include "breakpoint/breakpoint.hpp"
 #include "util/simple_options.hpp"
+#include "util/sndfile_utils.hpp" // TODO should not need
+
+static SndfileErr process_breakpoints_file( const std::string & infile ) noexcept {
+    auto result = breakpoint::parse_breakpoints( infile );
+    auto * err = std::get_if<breakpoint::parse_error>( &result );
+    if ( err ) {
+        std::cout << "Error " << to_string( err->code ) << " on line: " << err->line << std::endl;
+        return SndfileErr::CouldNotOpen;
+    }
+
+    auto & points = *std::get_if<std::vector<breakpoint::point>>( &result );
+    for ( auto && pt : points ) {
+        std::cout << "Point: " << pt.time_secs << " @ " << pt.value << std::endl;
+    }
+
+    auto && max_pt = breakpoint::max_point( begin( points ), end( points ) );
+    std::cout << "\nMax point: " << max_pt.time_secs << " @ " << max_pt.value << std::endl;
+    return SndfileErr::Success;
+}
 
 int main( int argc, char ** argv ) {
     simple_options::options opts{ "bkpts_driver" };
@@ -11,33 +30,5 @@ int main( int argc, char ** argv ) {
         .positional( "file", "Input file" )
         .parse( argc, argv );
 
-    if ( opts.has( "help" ) ) {
-        std::cout << opts;
-        return 0;
-    }
-
-    if ( opts.has( "file" ) ) {
-        std::ifstream infile( opts["file"].as<std::string>() );
-        auto result = breakpoint::parse_breakpoints( infile );
-        auto * err = std::get_if<breakpoint::parse_error>( &result );
-        if ( err ) {
-            std::cout << "Error "
-                      << std::underlying_type_t<breakpoint::parse_error::errc>( err->code )
-                      << " on line: " << err->line << std::endl;
-            return 1;
-        } else {
-            auto & points = *std::get_if<std::vector<breakpoint::point>>( &result );
-            for ( auto && pt : points ) {
-                std::cout << "Point: " << pt.time_secs << " @ " << pt.value << std::endl;
-            }
-
-            auto && max_pt = breakpoint::max_point( begin( points ), end( points ) );
-            std::cout << "\nMax point: " << max_pt.time_secs << " @ " << max_pt.value << std::endl;
-        }
-    } else {
-        std::cout << opts;
-        return 1;
-    }
-
-    return 0;
+    checked_invoke( opts, std::array{"file"}, &process_breakpoints_file );
 }
