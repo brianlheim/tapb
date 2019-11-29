0fb67dffed178573b5ba547c338339f646f862e3
Brian Heim <brianlheim@gmail.com>
2019-11-29 17:31:46 -0600

    fwd_copy: use make_in/out_handle

--- src/util/sndfile_utils.hpp
+++ src/util/sndfile_utils.hpp
@@ -26,63 +26,6 @@ Amplitude db_to_amp( Loudness ld ) {
     return std::pow( 10.0, ld / 20.0 );
 }
 
-// Tries to open an input and output file, then forwards the resulting handles and other arguments
-// to the given function. `F`'s type should be a callable `Ret ()( SndfileHandle&,
-// SndfileHandle&, Ts... )`
-template <typename F, typename... Ts>
-bool fwd_copy( F && func,
-               const std::string & from_path,
-               const std::string & to_path,
-               Ts &&... ts ) {
-    SndfileHandle from{ from_path, SFM_READ };
-    if ( from.error() != SF_ERR_NO_ERROR ) {
-        std::cout << "Could not open read file: " << from_path << std::endl;
-        return false;
-    }
-
-    SndfileHandle to{ to_path, SFM_WRITE, from.format(), from.channels(), from.samplerate() };
-    if ( to.error() != SF_ERR_NO_ERROR ) {
-        std::cout << "Could not open write file: " << to_path << std::endl;
-        return false;
-    }
-
-#ifndef NDEBUG
-    std::cout << from << "\n" << to << "\n";
-#endif
-
-    return func( from, to, std::forward<Ts &&>( ts )... );
-}
-
-bool scale_copy( SndfileHandle & from,
-                 SndfileHandle & to,
-                 const Amplitude amp_scale,
-                 const size_t bufsize = 1024 ) {
-    std::vector<float> floats( from.channels() * bufsize );
-
-    sf_count_t read = 0;
-    sf_count_t total_written = 0;
-    while ( ( read = from.readf( floats.data(), bufsize ) ) ) {
-        std::transform( floats.begin(), floats.begin() + ( read * from.channels() ), floats.begin(),
-                        [&amp_scale]( auto x ) { return x * amp_scale; } );
-        auto written = to.writef( floats.data(), read );
-        if ( written < read ) {
-            std::cout << "Error while writing (" << written << " written): " << to.strError()
-                      << std::endl;
-            return false;
-        }
-
-        total_written += written;
-    }
-
-    if ( total_written != from.frames() ) {
-        std::cout << "Could not read entire file: " << from.strError() << std::endl;
-        std::cout << "Read " << from.frames() << " | Wrote " << to.frames() << std::endl;
-        return false;
-    }
-
-    return true;
-}
-
 // Factory methods
 std::unique_ptr<SndfileHandle> make_input_handle( const std::string & path ) noexcept {
     auto handle = std::make_unique<SndfileHandle>( path, SFM_READ );
@@ -135,4 +78,47 @@ std::unique_ptr<SndfileHandle> make_output_handle( const std::string & path,
     }
 
     return out_handle;
+}
+
+// Tries to open an input and output file, then forwards the resulting handles and other arguments
+// to the given function. `F`'s type should be a callable `Ret ()( SndfileHandle&,
+// SndfileHandle&, Ts... )`
+template <typename F, typename... Ts>
+bool fwd_copy( F && func,
+               const std::string & from_path,
+               const std::string & to_path,
+               Ts &&... ts ) {
+    auto from = make_input_handle( from_path );
+    auto to = make_output_handle( to_path, from );
+    return from && to ? func( *from, *to, std::forward<Ts &&>( ts )... ) : false;
+}
+
+bool scale_copy( SndfileHandle & from,
+                 SndfileHandle & to,
+                 const Amplitude amp_scale,
+                 const size_t bufsize = 1024 ) {
+    std::vector<float> floats( from.channels() * bufsize );
+
+    sf_count_t read = 0;
+    sf_count_t total_written = 0;
+    while ( ( read = from.readf( floats.data(), bufsize ) ) ) {
+        std::transform( floats.begin(), floats.begin() + ( read * from.channels() ), floats.begin(),
+                        [&amp_scale]( auto x ) { return x * amp_scale; } );
+        auto written = to.writef( floats.data(), read );
+        if ( written < read ) {
+            std::cout << "Error while writing (" << written << " written): " << to.strError()
+                      << std::endl;
+            return false;
+        }
+
+        total_written += written;
+    }
+
+    if ( total_written != from.frames() ) {
+        std::cout << "Could not read entire file: " << from.strError() << std::endl;
+        std::cout << "Read " << from.frames() << " | Wrote " << to.frames() << std::endl;
+        return false;
+    }
+
+    return true;
 }
