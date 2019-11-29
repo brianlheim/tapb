f5421a8c23fd8309b09062fdc2508ea2886434e0
Brian Heim <brianlheim@gmail.com>
2019-11-29 15:50:50 -0600

    checked_invoke: consolidate

--- src/util/checked_invoke.hpp
+++ src/util/checked_invoke.hpp
@@ -3,20 +3,28 @@
 #include "util/simple_options.hpp"
 #include "util/sndfile_utils.hpp"
 
+#include <array>
 #include <iostream>
 
-template <typename F>
-[[nodiscard]] static int checked_invoke_in_out( simple_options::options & opts,
-                                                F && lambda ) noexcept {
+namespace detail {
+
+template <typename F, size_t N>
+[[nodiscard]] static int checked_invoke( simple_options::options & opts,
+                                         const std::array<const char *, N> & arg_names,
+                                         F && lambda ) noexcept {
     if ( opts.has( "help" ) ) {
         std::cout << opts;
         return 0;
     }
 
-    if ( opts.has( "input" ) && opts.has( "output" ) ) {
-        auto && input = opts["input"].as<std::string>();
-        auto && output = opts["output"].as<std::string>();
-        if ( lambda( input, output ) == SndfileErr::Success ) {
+    bool has_all = std::all_of( begin( arg_names ), end( arg_names ),
+                                [&opts]( auto * name ) { return opts.has( name ); } );
+    if ( has_all ) {
+        std::array<std::string, N> args;
+        for ( size_t i = 0; i < N; ++i ) {
+            args[i] = opts[arg_names[i]].template as<std::string>();
+        }
+        if ( apply( std::forward<F>( lambda ), args ) == SndfileErr::Success ) {
             return 0;
         } else {
             std::cout << "Operation failed." << std::endl;
@@ -28,26 +36,18 @@ template <typename F>
     }
 }
 
+} // namespace detail
+
+template <typename F>
+[[nodiscard]] static int checked_invoke_in_out( simple_options::options & opts,
+                                                F && lambda ) noexcept {
+    return detail::checked_invoke( opts, std::array{ "input", "output" },
+                                   std::forward<F>( lambda ) );
+}
+
 template <typename F>
 [[nodiscard]] static int checked_invoke_in_out_bkpts( simple_options::options & opts,
                                                       F && lambda ) noexcept {
-    if ( opts.has( "help" ) ) {
-        std::cout << opts;
-        return 0;
-    }
-
-    if ( opts.has( "input" ) && opts.has( "output" ) && opts.has( "breakpoints" ) ) {
-        auto && input = opts["input"].as<std::string>();
-        auto && output = opts["output"].as<std::string>();
-        auto && breakpoints = opts["breakpoints"].as<std::string>();
-        if ( lambda( input, output, breakpoints ) == SndfileErr::Success ) {
-            return 0;
-        } else {
-            std::cout << "Operation failed." << std::endl;
-            return 2;
-        }
-    } else {
-        std::cout << opts;
-        return 1;
-    }
+    return detail::checked_invoke( opts, std::array{ "input", "output", "breakpoints" },
+                                   std::forward<F>( lambda ) );
 }
